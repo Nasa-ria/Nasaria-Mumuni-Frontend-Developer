0ec1e2083008743cd4dfa60fa93af828b10d4858
ria-hue <mumuninasaria@gmail.com>
2023-11-17 20:44:39 -0700

    rewrote the controller function for search

--- Api/src/Controller/RocketController.php
+++ Api/src/Controller/RocketController.php
@@ -50,21 +50,24 @@ class RocketController{
     }
 
 
-    public function getRocket(ServerRequest $request, Response $response, $rocket_id)
-    {
 
-        try {
-            $rocketData = $this->rocketModel->findOne($rocket_id);
-            // Use your custom jsonResponse method
-            return $this->jsonResponse(['message' => 'Data fetched successfully', 'data' => $rocketData]);
-        } catch (\Exception $e) {
-            // Handle the exception, log, and return an error response
-            return $this->jsonResponse(['error' => $e->getMessage()], 500);
-        }
+    public function searchRocket(ServerRequest $request, Response $response)
+    {   
+        $queryParams = $request->getQueryParams();
+        // Extract filters from query parameters
+        $status = isset($queryParams['status']) ? $queryParams['status'] : null;
+        $originalLaunch = isset($queryParams['original_launch']) ? $queryParams['original_launch'] : null;
+        $type = isset($queryParams['type']) ? $queryParams['type'] : null;
+    try {
+
+        $rocketData = $this->rocketModel->search($status, $originalLaunch, $type);
+        // Send JSON response
+        return $this->jsonResponse(['message' => 'Data fetched successfully', 'data' => $rocketData]);
+    } catch (\Exception $e) {
+        // Handle the exception, log, and return an error response
+        return $this->jsonResponse(['error' => $e->getMessage()], 500);
     }
-
-
-
+    }
 
 
 
