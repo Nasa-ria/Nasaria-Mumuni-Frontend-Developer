8594b1b3e87d2295d8da521f3fb950f32188cb77
ria-hue <mumuninasaria@gmail.com>
2023-11-17 20:32:53 -0700

    created  a spaceXapi function to prevent repitition of curl(payload) in the model. and a construct for the url

--- Api/src/Model/Rockt.php
+++ Api/src/Model/Rockt.php
@@ -3,7 +3,28 @@ namespace App\Model;
 
 class Rocket{
 
+    private $apiUrl;
     
+    public function __construct()
+    {
+       $this->apiUrl = 'https://api.spacexdata.com/v3/rockets';
+    }
+
+
+    private function spaceXapi($param=""){
+        $ch = curl_init($this->apiUrl  .$param);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        // Check for cURL errors
+        if (curl_errno($ch)) {
+            throw new \Exception('Failed to fetch data from SpaceX API');
+        }
+        curl_close($ch);
+        return json_decode($data, true);
+    }
+
+
+
     public function findAll()
     {
         return $this->spaceXapi();
