64a9256f0111f5a47a40e2f7b5e86a0fc6b23e37
ria-hue <mumuninasaria@gmail.com>
2023-11-17 20:21:30 -0700

    call the  route and controller in index.php.setup routes for endpoints

--- Api/index.php
+++ Api/index.php
@@ -1,3 +1,45 @@
 <?php
 
 namespace App;
+
+use App\Model\Rocket;
+use App\Controller\RocketController;
+use Nyholm\Psr7\Factory\Psr17Factory;
+
+require_once './vendor/autoload.php';
+
+$psr17Factory = new Psr17Factory();
+$request = $psr17Factory->createServerRequest('GET', $_SERVER['REQUEST_URI']);
+$response = $psr17Factory->createResponse();
+
+$uri = $request->getUri()->getPath();
+
+$rocketModel = new Rocket();
+$controller = new RocketController($rocketModel);
+
+
+$segments = explode('/', trim($uri, '/'));
+
+if ($segments[0] == 'rockets') {
+    switch ($segments[1] ?? null) {
+        case 'search':
+            // Assuming it's a search request
+            $response=  $controller->searchRocket($request, $response);
+            break;
+        case null:
+            // Assuming it's a request for all rockets
+            $response=   $controller->listRockets($request, $response);
+            break;
+        default:
+            // Assuming it's a string identifier
+            $rocket_id = $segments[1];
+            $response=    $controller->getRocket($request, $response, $rocket_id);
+            break;
+    }
+} else {
+    // Handle other routes or return a default response if needed
+    echo 'Invalid endpoint';
+}
+
+// Output the response
+echo $response->getBody();
