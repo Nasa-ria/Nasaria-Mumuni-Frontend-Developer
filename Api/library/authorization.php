6e543794fdbbb70d7a8f54336d65a0c5794d053f
ria-hue <mumuninasaria@gmail.com>
2023-11-18 00:43:28 -0700

    created a  token for authentication

--- Api/library/authorization.php
+++ Api/library/authorization.php
@@ -1,1 +1,12 @@
 <?php
+
+use Firebase\JWT\JWT;
+
+$secretKey = 'your-secret-key';
+
+$userData = [
+    'sub' => 'anonymous', 
+];
+
+// Create a token
+$token = JWT::encode( $userData,$secretKey, 'HS256');
