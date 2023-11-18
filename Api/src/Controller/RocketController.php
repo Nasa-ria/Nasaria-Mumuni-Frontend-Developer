80c692ce3578f217cd319aecc1bb0fabdf3b6e1c
ria-hue <mumuninasaria@gmail.com>
2023-11-17 20:23:52 -0700

    controller function for listRockets.function to fetch all rockets

--- Api/src/Controller/RocketController.php
+++ Api/src/Controller/RocketController.php
@@ -1,4 +1,23 @@
 <?php
 
 namespace App\Controller;
-class RocketController{}
+class RocketController{
+
+    public function listRockets(ServerRequest $request, Response $response)
+    {
+        try {
+            $rocketData = $this->rocketModel->findAll();
+            return $this->jsonResponse(['message' => 'Data fetched successfully', 'data' => $rocketData]);
+        } catch (\Exception $e) {
+            // Handle the exception, log, and return an error response
+            return $this->jsonResponse(['error' => $e->getMessage()], 500);
+        }
+    }
+
+
+
+
+
+
+
+}
